7303a7df5a851a47e677237db62efd98991aee7e
kritikaarora <kritika2014arora@gmail.com>
2020-05-05 17:43:41 +0530

    added calendar_apis apps as a installable package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='mx-google-calendar',
+   version='1.0',
+   description='mx-google-calendar apis',
+   author='Kritika Arora',
+   author_email='kritika.arora@manprax.com',
+   packages=['calendar_apis'],  #same as name
+   install_requires=['google-api-python-client', 'google-auth-httplib2','google-auth-oauthlib'], #external packages as dependencies
+)
